a465e7e8fa2b33826a1eedccd62c525d1f039b8f
Angela Xiao <angelaxiao@fb.com>
2018-06-28 12:55:15 -0700

    Finished

--- app/src/main/java/peapod/angela/loginscreen/LoginActivity.java
+++ app/src/main/java/peapod/angela/loginscreen/LoginActivity.java
@@ -1,13 +1,34 @@
 package peapod.angela.loginscreen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @JavascriptInterface
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @JavascriptInterface
+    void onClick(View view){
+        String url = (String) view.getContentDescription();
+        WebView webView = findViewById(R.id.webview);
+        visible(webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+
+    @JavascriptInterface
+    void visible(WebView webView){
+        webView.setVisibility(View.VISIBLE);
     }
 }
